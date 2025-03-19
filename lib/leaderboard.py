e84ab58d157f3324a15fb0119be79d2e335e2e45
Jackson Majewski <jacksonmajewski@gmail.com>
2025-03-19 14:18:30 -0400

    Updated leaderboard embed to work multiline

--- lib/leaderboard.py
+++ lib/leaderboard.py
@@ -19,18 +19,21 @@ def get_leaderboard_embed(
         )
 
     # Build description using member sorted leaderboard
-    description = ""
+    # TODO: modify description based on which leaderboard is being shown
+    top = ""
+    lists = "" 
+    description = "Top Leaderboard"
+
+    embed = discord.Embed(title="Robin Leaderboard", description=description, color=discord.Color.from_str("#d15236"), timestamp=discord.utils.utcnow())
     for rank, entry in enumerate(leaderboard_data, start=1):
         # Attempt to get the member object from the guild.
         member = guild.get_member(int(entry["user_id"])) if guild else None
         if rank == 1:
             top_member = member
-        description += (
-            f"{rank}. **{member.display_name}**: {entry['total']} "
-            "<:tiny_robin:1351821803156279336>"
-        )
-
-    embed = discord.Embed(title="Robin Leaderboard", description=description)
+            top=f":crown: **{member.display_name}**: {entry['total']} <:tiny_winner_robin:1351970472916680744>"
+            continue
+        lists=f"{rank}. {member.display_name}: {entry['total']} <:tiny_robin:1351821803156279336>\n"
+    embed.add_field(name=top, value=lists, inline=False)
     embed.set_thumbnail(url=top_member.display_avatar.url)
 
     return embed
